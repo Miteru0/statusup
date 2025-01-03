00a3128c927b1c9edbb06b4dd50d4db6e014e927
Miteru0 <miteruolen@gmail.com>
2025-01-03 22:07:43 +0100

    added another error handling

--- src/main/java/com/statusup/statusup/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/statusup/statusup/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.statusup.statusup.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -10,6 +11,14 @@ import com.statusup.statusup.models.ErrorResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Handle Invalid JSON Body (HttpMessageNotReadableException)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleMassageNotReadableException(HttpMessageNotReadableException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
+    }
 
     // Handle AlreadyExistsException
     @ExceptionHandler(AlreadyExistsException.class)
